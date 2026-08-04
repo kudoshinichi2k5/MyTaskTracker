fe8c891b4d85c4c5cc516464d965fa28732c56a1
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-08-04 10:11:10 +0700

    chore(fe): remove orphaned environment.development.ts and unused component html files

--- Frontend/src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from './environment.model';
-
-export const environment: Environment = {
-  production: false,
-  authApi: 'http://localhost:5001/api/v1/auth',
-  taskApi: 'http://localhost:5002/api/v1'
-};
