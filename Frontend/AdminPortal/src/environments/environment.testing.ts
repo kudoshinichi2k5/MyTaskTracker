3f149fdfd4549063c18320fc9df68ed67561d514
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-08-11 13:13:20 +0700

    fix(frontend): point testing apps to IIS hostnames

--- Frontend/AdminPortal/src/environments/environment.testing.ts
+++ Frontend/AdminPortal/src/environments/environment.testing.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  authApi: 'http://localhost:5001/api/v1',
-  taskApi: 'http://localhost:5002/api/v1',
-  notificationApi: 'http://localhost:5003/api/v1'
+  authApi: 'http://auth.testing.local/api/v1',
+  taskApi: 'http://api.testing.local/api/v1',
+  notificationApi: 'http://notifications.testing.local/api/v1'
 };
