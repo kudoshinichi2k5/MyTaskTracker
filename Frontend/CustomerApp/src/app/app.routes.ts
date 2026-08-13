5928d681e65df989430749496e679f572bd03cc3
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-08-13 14:25:37 +0700

    fix(customer-ui): path

--- Frontend/CustomerApp/src/app/app.routes.ts
+++ Frontend/CustomerApp/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-
 import { LoginComponent } from './features/login/login.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
+
   {
     path: '',
     canActivate: [authGuard],
@@ -19,33 +19,37 @@ export const routes: Routes = [
       {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'tasks'
+        redirectTo: 'tasks',
       },
+
       {
         path: 'tasks',
         loadComponent: () =>
           import('./features/tasks/task-page.component').then(
             (m) => m.TaskPageComponent
-          )
+          ),
       },
+
       {
         path: 'projects',
         loadComponent: () =>
           import('./features/projects/project-page.component').then(
             (m) => m.ProjectPageComponent
-          )
+          ),
       },
+
       {
         path: 'comments',
         loadComponent: () =>
           import('./features/comments/comment-page.component').then(
             (m) => m.CommentPageComponent
-          )
-      }
-    ]
+          ),
+      },
+    ],
   },
+
   {
     path: '**',
-    redirectTo: 'tasks'
-  }
+    redirectTo: 'tasks',
+  },
 ];
