6fb9e34425ec6c3df2d23eb004df80226695b303
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-08-13 16:44:43 +0700

    fix(admin): register users and roles routes

--- Frontend/AdminPortal/src/app/app.routes.ts
+++ Frontend/AdminPortal/src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const routes: Routes = [
     path: 'forbidden',
     loadComponent: () =>
       import('./components/forbidden/forbidden.component')
-        .then((m) => m.ForbiddenComponent)
+        .then(m => m.ForbiddenComponent)
   },
 
   {
@@ -34,16 +34,36 @@ export const routes: Routes = [
     path: 'projects',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./features/projects/project-list/project-list.component')
-        .then((m) => m.ProjectListComponent)
+      import(
+        './features/projects/project-list/project-list.component'
+      ).then(m => m.ProjectListComponent)
   },
 
   {
     path: 'comments',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./features/comments/comment-list/comment-list.component')
-        .then((m) => m.CommentListComponent)
+      import(
+        './features/comments/comment-list/comment-list.component'
+      ).then(m => m.CommentListComponent)
+  },
+
+  {
+    path: 'users',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import(
+        './features/users/user-management.component'
+      ).then(m => m.UserManagementComponent)
+  },
+
+  {
+    path: 'roles',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import(
+        './features/roles/role-management.component'
+      ).then(m => m.RoleManagementComponent)
   },
 
   {
