1518518e33e3ffa6ad75c07b595009c4bf4e944d
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-08-13 16:03:55 +0700

    feat(admin): add project and comment routes

--- Frontend/AdminPortal/src/app/app.routes.ts
+++ Frontend/AdminPortal/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './features/login/login.component';
+
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+
   {
     path: 'login',
     component: LoginComponent
@@ -13,62 +21,33 @@ export const routes: Routes = [
     path: 'forbidden',
     loadComponent: () =>
       import('./components/forbidden/forbidden.component')
-        .then(m => m.ForbiddenComponent)
+        .then((m) => m.ForbiddenComponent)
   },
 
   {
-    path: '',
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'projects',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./layout/admin-shell.component')
-        .then(m => m.AdminShellComponent),
+      import('./features/projects/project-list/project-list.component')
+        .then((m) => m.ProjectListComponent)
+  },
 
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'overview'
-      },
-
-      {
-        path: 'overview',
-        loadComponent: () =>
-          import('./features/overview/overview-page.component')
-            .then(m => m.OverviewPageComponent)
-      },
-
-      {
-        path: 'users',
-        loadComponent: () =>
-          import('./features/users/user-management.component')
-            .then(m => m.UserManagementComponent)
-      },
-
-      {
-        path: 'roles',
-        loadComponent: () =>
-          import('./features/roles/role-management.component')
-            .then(m => m.RoleManagementComponent)
-      },
-
-      {
-        path: 'projects',
-        loadComponent: () =>
-          import('./features/projects/project-management.component')
-            .then(m => m.ProjectManagementComponent)
-      },
-
-      {
-        path: 'comments',
-        loadComponent: () =>
-          import('./features/comments/comment-management.component')
-            .then(m => m.CommentManagementComponent)
-      }
-    ]
+  {
+    path: 'comments',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/comments/comment-list/comment-list.component')
+        .then((m) => m.CommentListComponent)
   },
 
   {
     path: '**',
-    redirectTo: 'overview'
+    redirectTo: 'dashboard'
   }
 ];
