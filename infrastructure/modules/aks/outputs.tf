9a7de7b022febc1ce39965b950094084cf47379d
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-09-24 12:21:49 +0700

    feat(infra): add output of provider kubernetes

--- infrastructure/modules/aks/outputs.tf
+++ infrastructure/modules/aks/outputs.tf
@@ -2,6 +2,30 @@ output "aks_id" {
   value = azurerm_kubernetes_cluster.aks.id
 }
 
+output "host" {
+  description = "Kubernetes API server endpoint"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive   = true
+}
+
+output "client_certificate" {
+  description = "Base64-encoded Kubernetes client certificate"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "Base64-encoded Kubernetes client key"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64-encoded Kubernetes cluster CA certificate"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  sensitive   = true
+}
+
 # Output này cực kỳ quan trọng cho Task 6 (Cấp quyền kéo image từ ACR)
 output "kubelet_identity_object_id" {
   value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
