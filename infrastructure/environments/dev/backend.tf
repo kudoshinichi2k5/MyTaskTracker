e17a110cdd4e45ffa10a9fd5afdbd5b2ef20dc18
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-09-25 16:48:28 +0700

    ci(terraform): fix init 403 error by injecting oidc config dynamically

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -4,9 +4,5 @@ terraform {
     storage_account_name = "tfstate4459"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-
-    # Ép sử dụng token OIDC từ GitHub Actions
-    use_oidc             = true
-    use_azuread_auth     = true
   }
 }
