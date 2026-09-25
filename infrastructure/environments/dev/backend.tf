98b035398f747683a340eccfcb87d83fc29852a8
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-09-25 16:44:44 +0700

    fix(infra): force azure ad oidc authentication for terraform state backend to resolve 403 listkeys error

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -4,9 +4,9 @@ terraform {
     storage_account_name = "tfstate4459"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-  }
 
-  # THÊM 2 DÒNG NÀY: Ép sử dụng token OIDC từ GitHub Actions
+    # Ép sử dụng token OIDC từ GitHub Actions
     use_oidc             = true
     use_azuread_auth     = true
+  }
 }
